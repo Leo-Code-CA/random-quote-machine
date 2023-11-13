75086a2898885c2ec040f35a74fef972e61e0f85
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2023-11-13 15:45:55 -0500

    Page flip effect

--- js/main.js
+++ js/main.js
@@ -2,27 +2,25 @@ $(document).ready(function() {
 
     const API_URL = "https://api.quotable.io/quotes/random?minLength=100&maxLength=200";
 
-    const apiCall = () => {
-        return $.get( API_URL, function( data ) {
-            $(".box__text").html(data[0].content);
-            $(".box__author").html(data[0].author);
-         });
+    $('.box__button--next').click(function() {
+        $('.box__sheet--current').addClass('flip');
+        setTimeout(changeClass, 1000);
+    });
+
+    const changeClass = () => {
+        $('.box__sheet').toggleClass('box__sheet--current');
+        $('.box__sheet').toggleClass('box__sheet--next');
+        $('.box__sheet--next').removeClass('flip');
+        contentUpdate();
     }
 
-    apiCall();
-    $(".box__button").click(apiCall);
-
-    // const animatedSteam = () => {
-    //     $('.stopOne')
-    //     .animate({
-    //         stopColor: 'pink'
-    //     }, 3000)
-    //     .animate({
-    //         stopColor: 'red'
-    //     }, 3000, animatedSteam)
-    // }
-
-    // animatedSteam();
-
+    const contentUpdate = () => {
+        $.get( API_URL, function( data ) {
+        $(".box__sheet--next .box__text").html(data[0].content);
+        $(".box__sheet--next .box__author").html(data[0].author);
+        });
+    }
 
 });
+
+    
