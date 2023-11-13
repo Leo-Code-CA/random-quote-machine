66b5bc5f44a45539626a35c3b53dac9dca49ea3e
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2023-11-13 14:06:24 -0500

    Coffee steam animation

--- js/main.js
+++ js/main.js
@@ -2,12 +2,27 @@ $(document).ready(function() {
 
     const API_URL = "https://api.quotable.io/quotes/random?minLength=100&maxLength=200";
 
-    $(".box__button").click(function(){
-        $.get( API_URL, function( data ) {
+    const apiCall = () => {
+        return $.get( API_URL, function( data ) {
             $(".box__text").html(data[0].content);
             $(".box__author").html(data[0].author);
          });
-    })
+    }
+
+    apiCall();
+    $(".box__button").click(apiCall);
+
+    // const animatedSteam = () => {
+    //     $('.stopOne')
+    //     .animate({
+    //         stopColor: 'pink'
+    //     }, 3000)
+    //     .animate({
+    //         stopColor: 'red'
+    //     }, 3000, animatedSteam)
+    // }
+
+    // animatedSteam();
 
 
 });
